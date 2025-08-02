131026017c62dfc1a43e00af24218845a2eb93ef
Eduardo <heyimedd@gmail.com>
2025-08-02 02:03:11 -0600

    fix: Actualizar URI del autor y dominio de texto en el archivo del plugin

--- ewm-modal-cta.php
+++ ewm-modal-cta.php
@@ -7,10 +7,10 @@
  * Requires at least: 5.8
  * Requires PHP:      7.4
  * Author:            Equipo EWM
- * Author URI:        https://ewm.com
+ * Author URI:        https://especialistaenwp.com
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain:       ewm-modal-cta
+ * Text Domain:       ewm-modal
  * Domain Path:       /languages
  *
  * @package           EWM_Modal_CTA
